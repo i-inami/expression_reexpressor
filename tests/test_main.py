029f9ae4aed28d47b0a0caac715b44315f0c58b6
Inami <inamiirin@gmail.com>
2026-09-05 15:24:44 +0900

    feature: solve equations and return expression

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,19 @@ from app.main import app
 client = TestClient(app)
 
 
-def test_reexpress():
+def test_reexpress_single_solution():
     r = client.post("/reexpress", json={"expression": "y=a*x+b", "variable": "x"})
     assert r.status_code == 200
-    assert r.json() == {"expression": "(y - b)/a"}
+    assert r.json() == {"expressions": ["(y - b)/a"]}
+
+
+def test_reexpress_multiple_solutions():
+    r = client.post("/reexpress", json={"expression": "x**2=4", "variable": "x"})
+    assert r.status_code == 200
+    assert r.json() == {"expressions": ["-2", "2"]}
+
+
+def test_reexpress_no_solution():
+    r = client.post("/reexpress", json={"expression": "x=x+1", "variable": "x"})
+    assert r.status_code == 200
+    assert r.json() == {"expressions": []}
